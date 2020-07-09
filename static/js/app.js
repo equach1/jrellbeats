77392a43b54b4d45c261c85e2a780d02230c25d8
Brandon <bkuon@uco.edu>
2020-07-09 15:54:12 -0500

    Additional updates on Modal Edit 3

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,7 @@
 //modal items
-const modal = document.getElementById('email-modal');
-const openBtn = document.querySelector('.main-btn');
-const closeBtn = document.querySelector('.close-btn');
-
-openBtn.addEventListener('click', () =>{
-    modal.style.display = 'block';
-})
-
-closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-})
-
-window.addEventListener('click', (e) => {
-    if(e.target == modal){
-        modal.style.display = 'none';
-    }
-})
+document.getElementById('button').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
