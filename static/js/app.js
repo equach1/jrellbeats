dbf53a5f800c66e1494a1969fb75fb81f3e467ef
Brandon <bkuon@uco.edu>
2020-07-09 18:10:36 -0500

    New Modal Edit Again Pt 4

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 //modal items
 document.getElementById('button').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.modal').style.display = 'flex';
 });
 document.querySelector('.close').addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'none';
+    document.querySelector('.modal').style.display = 'none';
 });
